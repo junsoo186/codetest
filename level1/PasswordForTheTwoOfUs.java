27277d53c1cb1a27523fe80b7f62256572067b87
junsoo <junsoo186@gmail.com>
2024-12-24 18:12:29 +0900

    12/24

--- level1/PasswordForTheTwoOfUs.java
+++ level1/PasswordForTheTwoOfUs.java
@@ -1,25 +1,29 @@
-class Solution {
+class PasswordForTheTwoOfUs {
     public String solution(String s, String skip, int index) {
         StringBuilder answer = new StringBuilder();
         char[] findbox = new char[26];
+
         for (int i = 0; i < 26; i++) {
             findbox[i] = (char) ('a' + i);
         }
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-              for (int j = 0; j < skip.length(); j++) {
-                int started= c - 'a';
-                int skiped = skip.charAt(j) - 'a';
-                int sum = started + index;
-                if (started < skiped && sum > skiped) {
-                    ++ sum ;
+            int position = c - 'a';
+            int steps = 0;
 
-                }
-                sum %= 26;
+            while (steps < index) {
+                position = (position + 1) % 26;
+                char nextChar = findbox[position];
                 
-                answer.append(findbox[sum]);
+                if (skip.indexOf(nextChar) == -1) {
+                    steps++;
+                }
             }
+
+            answer.append(findbox[position]);
         }
+
         return answer.toString();
     }
 }
