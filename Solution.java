da4b8b67cec3567e46b1e02ff8e5a8123a9f2c80
junsoo <junsoo186@gmail.com>
2025-01-03 14:24:12 +0900

    01/03-1

--- Solution.java
+++ Solution.java
@@ -1,16 +1,32 @@
-class Solution {
-    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = new int[2];
-        int numer = numer1 * denom2 + numer2 * denom1;
-        int denom = denom1 * denom2;
-        int gcd = 1;
-        for (int i = 1; i <= numer && i <= denom; i++) {
-            if (numer % i == 0 && denom % i == 0) {
-                gcd = i;
+public class Solution {
+    public int solution(int[][] board) {
+        
+        int size1 = board.length;
+        int size2 = board[0].length;
+        int answer = size1*size2;
+        int count = 0;
+        boolean[][] boomBox = new boolean[size1][size2];
+        int[] dx = new int[]{-1,0,1,-1,0,1,-1,0,1};
+        int[] dy = new int[]{-1,-1,-1,0,0,0,1,1,1};
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
+                 int isBoom = board[i][j];
+                 if(isBoom == 1){
+                    for (int k = 0; k < 9; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if (nx >= 0 && nx < size1 && ny >=0 && ny< size2){
+                            if (  boomBox[nx][ny] == true  ) {
+                                count--;
+                            }
+                            boomBox[nx][ny] = true; 
+                            count++;
+                        }
+                    }
+                 }
             }
+            
         }
-        answer[0] = numer / gcd;
-        answer[1] = denom / gcd;
-        return answer;
+        return answer -=count;
     }
 }
