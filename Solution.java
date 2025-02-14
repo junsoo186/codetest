04b50483ee95aeaa89908c207a53be658049d9eb
junsoo <junsoo186@gmail.com>
2025-02-14 23:51:31 +0900

    02/14

--- Solution.java
+++ Solution.java
@@ -1,26 +1,29 @@
-import java.util.Stack;
-
 class Solution {
-
-    public int solution(int[] order) {
-        int answer = 0;
-        int count = 1; // 현재 택배 번호
-        Stack<Integer> sub = new Stack<>(); // 보조 스택
-
-        for (int box : order) {
-            while (count <= box) { // 컨베이어에서 택배를 꺼내기
-                sub.push(count);
-                count++;
-            }
-
-            if (!sub.isEmpty() && sub.peek() == box) {
-                sub.pop();
-                answer++;
-            } else {
-                break;
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i = 0; i < prices.length ; i ++ ){
+                int count = 0;            
+            for(int j = 1; j < prices.length ; j++ ){
+                int start = prices[i + count] ;
+                if( start < prices[j]){
+                    count++;
+                }else{
+                    if(count == 0){
+                        answer[i] = 1;    
+                    }
+                    answer[i]=count + 2;    
+                    count = 0;
+                    break;            
+        
+                }
+                
             }
         }
-
         return answer;
+    }
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 2, 3} ;
+        Solution calc = new Solution();
+        System.out.println(calc.solution(a));
     }
 }
