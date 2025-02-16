a402193d147c63774d68672bb0d89fb8d2f634db
junsoo <junsoo186@gmail.com>
2025-02-16 14:43:32 +0900

    02/16

--- Solution.java
+++ Solution.java
@@ -1,29 +1,55 @@
 class Solution {
-    public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        for(int i = 0; i < prices.length ; i ++ ){
-                int count = 0;            
-            for(int j = 1; j < prices.length ; j++ ){
-                int start = prices[i + count] ;
-                if( start < prices[j]){
-                    count++;
-                }else{
-                    if(count == 0){
-                        answer[i] = 1;    
-                    }
-                    answer[i]=count + 2;    
-                    count = 0;
-                    break;            
-        
-                }
-                
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+        boolean chulSu = false;
+        boolean youngHee = false;
+        int countC = 0;
+        int countY = 0;
+        int sizes = arrayA.length;
+        int x = arrayA[0];
+        int y = arrayB[0];
+        while (countC < sizes && countY < sizes && countC != 0 && countY != 0 ) { 
+            if(arrayA[countC]%x == 0){
+                countC++;
+            }else{
+                countC = 0;
+                x--;
+            }
+            if (countC == sizes) {
+                chulSu = true;
+            }
+            if(arrayB[countY]%y == 0){
+                countY++;
+            } else{
+                countY = 0; 
+                y--;
+            }
+
+            if (countY == sizes) {
+                youngHee = true;
             }
         }
+        
+        if(chulSu == true && youngHee == false){
+            for(int i = 0; i < sizes; i++ ){
+                if (arrayB[i]%x == 0){
+                    chulSu = false;
+                    break;
+                }
+            }
+            answer = x;
+        }else if( chulSu == false&&youngHee == true){
+            for(int i = 0; i < sizes; i++ ){
+                if (arrayA[i]%y == 0){
+                    youngHee = false;
+                    break;
+                }
+            }
+            answer = y;
+        
+           }else{
+            answer = 0;
+           }
         return answer;
-    }
-    public static void main(String[] args) {
-        int[] a = {1, 2, 3, 2, 3} ;
-        Solution calc = new Solution();
-        System.out.println(calc.solution(a));
     }
 }
